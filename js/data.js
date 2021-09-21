5ee86513c9bff913adcd54648802285777b8a7e1
bogoreh <76124689+bogoreh@users.noreply.github.com>
2021-09-21 05:42:47 +0300

    The first commit

---
+++ js/data.js
@@ -0,0 +1,13 @@
+const candidates = [{
+        id: "trump",
+        name: "Donald Trump",
+        party: "Republican Party",
+        votes: 260,
+    },
+    {
+        id: "biden",
+        name: "Joe Biden",
+        party: "Democratic Party",
+        votes: 260,
+    }
+]
